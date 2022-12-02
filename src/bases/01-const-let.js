3e3ae5307bfdeeb0311b03998911190c9d25190e
Jose Zamora <azamor4@gmail.com>
2022-12-01 20:11:54 -0600

    Primer commit, curos react

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,19 @@
+
+// Variables y Constantes
+
+const nombre = 'Jose';
+const apellido = 'Zamora';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre,apellido,valorDado);
+
+if(true){
+    const nombre = 'Miguel';
+
+    let valorDado = 6;
+    console.log(nombre);
+}
+console.log(valorDado);
+
